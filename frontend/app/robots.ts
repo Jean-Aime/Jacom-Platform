43bd232df8f9837a9bb9c6b9a5f0b4bbfade08a9
Jean-Aime <baraime450@gmail.com>
2026-02-20 17:50:59 +0200

    root&sitemap

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/admin/', '/api/']
     },
-    sitemap: 'https://jas.com/sitemap.xml'
+    sitemap: 'https://jacom-platform.vercel.app/sitemap.xml'
   };
 }
